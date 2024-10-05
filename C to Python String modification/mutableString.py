a2c1ad7fdd21211d572959f820543a2bf5f5124d
Saiful Islam Reyad <riyadsaif18@gmail.com>
2024-10-06 02:27:45 +0600

    changed file structure

--- C to Python String modification/mutableString.py
+++
@@ -1,8 +0,0 @@
-# we can do the same thing as L[i]='z' can do 
-# L="abcccba"
-# L[2]='k'
-# L=L.replace(L[2],"k",1)
-# print(L) #output: abkccb
-L = "abcccba"
-L = L[:2] + 'x' + L[3:]
-print(L)  # Output: abxccba
